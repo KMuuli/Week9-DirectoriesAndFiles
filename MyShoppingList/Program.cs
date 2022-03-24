58c6cef2f8ff5a2a6f9e97d09c9d56be1bffb34f
kaspar muuli <kasparmuuli@gmail.com>
2022-03-24 17:26:27 +0200

    add new projects

---
+++ MyShoppingList/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MyShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileLocation = @"C:\Users\37253\samples\ShoppingList";
+            string fileName = @"\\myshoppinglist.txt";
+
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+            List<string> myShoppingList = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add a wish? Y/N");
+                char userinput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if(userinput == 'y')
+                {
+                    Console.WriteLine("Enter your wish:");
+                    string userWish = Console.ReadLine();
+                    myShoppingList.Add(userWish);
+                }
+                else
+                {
+                    loopActive = false;
+                    Console.WriteLine("Take Care!");
+                }
+            }
+
+            Console.Clear();
+
+            foreach(string wish in myShoppingList)
+            {
+                Console.WriteLine($"You wish: {wish}");
+            }
+
+            File.WriteAllLines($"{fileLocation}{fileName}", myShoppingList);
+        }
+    }
+}
